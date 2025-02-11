6be4926b02194c905d570b2e3d39dd6d7b855d48
JackJ24 <2202601@spsd.sk.ca>
2025-02-11 12:14:26 -0600

    Start Of Interactive Stagr

---
+++ Projects/Interactive Stage/sketch.js
@@ -0,0 +1,19 @@
+// Interactive Stage
+// Jack
+// Feb 11th 2025
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+//setup
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+}
+
+//frames
+function draw() {
+  background(220);
+  fill(255, 155, 100);
+  rect(width / 2, height, 1000, height * .30);
+}
