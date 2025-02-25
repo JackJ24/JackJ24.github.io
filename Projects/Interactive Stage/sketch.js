01c7abf9ca2dbaa8760ef12925beb3cbc22a1e6f
JackJ24 <2202601@spsd.sk.ca>
2025-02-25 12:15:40 -0600

    Added Sun, Charater, and Movement

--- Projects/Interactive Stage/sketch.js
+++ Projects/Interactive Stage/sketch.js
@@ -5,49 +5,109 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+
+
+
 //setup
 function setup() {
   
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
+
   
 }
 
 //frames
 function draw() {
   background(200);
-  
-  
-  ground();
   mountians();
+  theGround();
+  theSun();
+  playChar();
+  
 }
 
 function scene(){
-  ground();
+  theGround();
   background();
 
 }
 
-function ground(){
+function theGround(){
   fill(25, 155, 0);
   rect(width / 2, height, width, height * .30);
 }
 
+//mountians
 function mountians(){
+  noStroke();
+  //mountain 1
+  fill(41, 40, 40);
+  triangle(200, 400, 0, 800, 400, 800);
+ 
+  //peak
+  stroke(0);
+  fill(255, 255, 255);
+  triangle(200, 400, 150, 500, 250, 500);
+
+
+  //mountian 2
+  noStroke();
+  fill(41, 40, 40);
+  triangle(400, 200, 100, 800, 800, 800);
   
-  fill(41, 40, 40);
-  triangle(200, 400, 0, height, 400, 800);
-
-
-  fill(0, 0, 0);
-  triangle(200, 400, 145, 500, 250, 500);
-
-  fill(41, 40, 40);
-  triangle(400, 200, 100, height, 800, 800);
-
-  fill(0, 0, 0);
-  triangle(400, 200, 295, 400, 535, 400);
+  //peak
+  stroke(0);
+  fill(255, 255, 255);
+  triangle(400, 200, 300, 400, 535, 400);
 }
+
+function theSun(){
+  
+  //colour and border
+  stroke(0);
+  fill(243, 156, 18);
+
+  circle(100, 100, height*0.30);
+}
+
+
+let charLoc = 100;
+function playChar(){
+
+  
+  charMove();
+
+  circle(charLoc, height * 0.85, height * 0.10)
+
+  fill(255, 255, 255);
+  circle(charLoc-20, height * 0.80, height * 0.05)
+  circle(charLoc+20, height * 0.80, height * 0.05)
+
+  fill(0, 0, 0);
+  circle(charLoc-20, height * 0.80, height * 0.02)
+  circle(charLoc+20, height * 0.80, height * 0.02)
+
+  fill(255, 0, 0, )
+  ellipse(charLoc, height*0.85, height * 0.08, height * 0.05)
+
+
+}
+
+
+function charMove(){
+  if (keyIsDown(LEFT_ARROW)){
+    charLoc-= 10;
+  }
+
+  if (keyIsDown(RIGHT_ARROW)){
+    charLoc+= 10;
+  }
+
+}
+
+
 
 
 
