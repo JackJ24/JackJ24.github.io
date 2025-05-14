8b0e2bac4c56e008fafb5c541cf49c8eba455997
JackJ24 <2202601@spsd.sk.ca>
2025-05-14 12:17:24 -0600

    Problems
    Tried to make a animation with a model in blender. It didnt work. still trying to make a roll button

--- final/Player.gd
+++ final/Player.gd
@@ -4,6 +4,7 @@ const MAX_HEALTH = 100
 var health = MAX_HEALTH
 var SPEED = 2.0
 const JUMP_VELOCITY = 4.5
+var roll_magnitude = 30
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -41,6 +42,15 @@ func movement(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		
+func roll_char():
+	if Input.is_action_pressed("Roll Button"):
+		if $roll_window.is_stopped():
+			$roll_window.start()
+	if Input.is_action_just_released("Roll Button"):
+		if $roll_window.is_stopped():
+			velocity = direction * roll_magnitude
+			$Node/roll_window.stop()
+			
 
 func RotateChar():
 	if Input.is_key_pressed(KEY_LEFT):
